32acfb83ac2136e4310c3078f94bd6a0b4174eaa
Дмитрий Дрёмин <dremin.da24108292@gmail.com>
2024-03-24 20:35:52 +0300

    first HW 13.1 in module OOP complete

--- src/item.py
+++ src/item.py
@@ -1,3 +1,11 @@
+class InstantiateCSVError(Exception):
+    def __init__(self):
+        self.error = 'Файл item.csv поврежден'
+
+    def __str__(self):
+        return self.error
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,7 +21,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +33,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,18 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item, InstantiateCSVError
+from src.phone import Phone
+from src.hard import Hard
+import os
+import pytest
+
+
+def test_calculate_total_price():
+    item = Item("Shirt", 20.0, 5)
+    assert item.calculate_total_price() == 100.0
+
+
+def test_apply_discount():
+    item = Item("Shirt", 20.0, 5)
+    Item.pay_rate = 0.9
+    item.apply_discount()
+    assert item.price == 18.0
